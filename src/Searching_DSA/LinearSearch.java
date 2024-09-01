eff6992cd2b9b6cdc2420785dfcfcd0cee34dac3
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-09-01 22:35:45 +0530

    Linear Search

--- src/Searching_DSA/LinearSearch.java
+++ src/Searching_DSA/LinearSearch.java
@@ -1,5 +1,26 @@
 package Searching_DSA;
-
-public class LinearSearch {
-
+import java.util.*;
+public class LinearSearch 
+{
+	public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int arr[]=new int[n];
+	for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+	
+	int target=sc.nextInt();
+	boolean isFound=false;
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]==target)
+		{
+			System.out.println("Element Fount at index : "+i);
+			isFound=true;
+			break;
+		}
+	}
+	if(!isFound) {
+		System.out.println("Element not Found");
+	}
+}
 }
