f98402243ac7f8091a806d13b9a6483fba8cee94
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-09-01 23:02:08 +0530

    Binary Search

--- src/Searching_DSA/BinarySearch.java
+++ src/Searching_DSA/BinarySearch.java
@@ -1,5 +1,39 @@
 package Searching_DSA;
-
+import java.util.*;
 public class BinarySearch {
+	private static int search(int arr[],int target)
+	{
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end) {
+			int mid=(start+end)/2;
+			if(arr[mid]==target) {
+				return mid;
+			}
+			else if(arr[mid]>target) {
+				end=mid-1;
+			}
+			else {
+				start=mid+1;
+			}
+		}
+		return -1;
+	}
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+		int target=sc.nextInt();
+		int result=search(arr,target);
+		if(result!=-1) {
+			System.out.println("Element found at index : "+ result);
+		}
+		else
+		{
+			System.out.println("Element not found");
+		}
+	}
 
 }
