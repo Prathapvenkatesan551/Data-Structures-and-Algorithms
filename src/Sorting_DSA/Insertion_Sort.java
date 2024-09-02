5c2f4d0dca21576b5e453e6fc185989a6da2bbe3
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-09-02 11:17:41 +0530

    Sorting

---
+++ src/Sorting_DSA/Insertion_Sort.java
@@ -0,0 +1,31 @@
+package Sorting_DSA;
+import java.util.*;
+public class Insertion_Sort
+{
+	private static void sort(int arr[]) 
+	{
+		for(int i=1;i<arr.length;i++)
+		{
+			int temp=arr[i];
+			int j=i-1;
+			while(j>=0) {
+				if(arr[j]>temp)
+				{
+					arr[j+1]=arr[j];
+					j--;
+				}
+			}
+			arr[j+1]=temp;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int arr[]=new int[n];
+	for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+	sort(arr);
+	
+	}
+}
