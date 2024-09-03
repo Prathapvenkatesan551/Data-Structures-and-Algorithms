308e6052b94fa07916c89e27cf8848bdd3a3ddc9
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-09-03 12:55:54 +0530

    Merge sort

--- src/Sorting_DSA/Bubble_Sort.java
+++ src/Sorting_DSA/Bubble_Sort.java
@@ -26,11 +26,7 @@ public class Bubble_Sort {
 		System.out.println(Arrays.toString(arr));
 	}
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int arr[]=new int[n];
-		for(int i=0;i<n;i++) arr[i]=sc.nextInt();
-		sort(arr);
+		
 
 	}
 
---
+++ src/Sorting_DSA/Merge_Sort.java
@@ -0,0 +1,44 @@
+package Sorting_DSA;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Merge_Sort {
+	private static int[] sort(int arr[]){
+		if(arr.length==1) return arr;
+		int mid=arr.length/2;
+		
+		int left[]=sort(Arrays.copyOfRange(arr, 0, mid));
+		int right[]=sort(Arrays.copyOfRange(arr, mid, arr.length));
+		
+		return merge(left,right);
+	}
+	private static int[] merge(int left[],int right[])
+	{
+		int arr[]=new int[left.length+right.length];
+		int i=0;
+		int j=0;
+		int index=0;
+		while(i<left.length && j<right.length) {
+			if(left[i]<right[j]) arr[index++]=left[i++];
+			else arr[index++]=right[j++];
+		}
+		if(i<left.length) {
+			while(i<left.length) arr[index++]=left[i++];
+		}
+		else{
+			while(j<right.length) arr[index++]=right[j++];
+		}
+		
+		return arr;
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+//		int result=merge(arr);
+		System.out.println(Arrays.toString(sort(arr)));
+	}
+
+}
