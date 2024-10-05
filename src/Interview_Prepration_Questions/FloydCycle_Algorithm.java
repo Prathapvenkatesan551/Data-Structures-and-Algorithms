3709b4a5312d8995df3aaeaba59c85389d569280
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-05 23:24:07 +0530

    Floyd Algorithm

--- src/Interview_Prepration_Questions/FloydCycle_Algorithm.java
+++ src/Interview_Prepration_Questions/FloydCycle_Algorithm.java
@@ -2,9 +2,23 @@ package Interview_Prepration_Questions;
 
 public class FloydCycle_Algorithm {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public boolean hasCycle(Node head) {
 
+	        Node n1=head;
+	        Node n2=head;
+	        while(n2!=null && n2.next!=null)
+	        {
+	            n1=n1.next;
+	            n2=n2.next.next;
+	            if(n1==n2) return true;
+	           
+	        }
+	        return false;
+	        
+	        
+	    }
+	public static void main(String[] args) {
+		
 	}
 
 }
