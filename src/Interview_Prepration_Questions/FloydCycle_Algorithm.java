574faf3094022943284f2c9284b18cbfbddb1aa0
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-10 20:53:39 +0530

    Remove loop in Linkedlist

--- src/Interview_Prepration_Questions/FloydCycle_Algorithm.java
+++ src/Interview_Prepration_Questions/FloydCycle_Algorithm.java
@@ -1,6 +1,29 @@
 package Interview_Prepration_Questions;
 
 public class FloydCycle_Algorithm {
+	public void removeCycle(Node head) {
+		Node n1=head;
+        Node n2=head;
+        Node prev=null;
+        while(n2!=null && n2.next!=null)
+        {
+            n1=n1.next;
+            n2=n2.next.next;
+            if(n1==n2)
+            	{
+            	n1=head;
+            	while(n1!=n2) {
+            		prev=n2;
+            		n1=n1.next;
+            		n2=n2.next	;
+            	}
+            	prev.next=null;
+            	return n1;
+            	}
+           
+        }
+        return null;
+	}
 
 	 public boolean hasCycle(Node head) {
 
