2ff4261fe3c427ff665b0ee39cfc594302107b22
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-20 22:38:50 +0530

    Build files

---
+++ src/Graph_DSA/BFS.java
@@ -0,0 +1,10 @@
+package Graph_DSA;
+
+public class BFS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Graph_DSA/DFS.java
@@ -0,0 +1,5 @@
+package Graph_DSA;
+
+public class DFS {
+
+}
